85cbf842923a6d0515c9ef05cd7b874dc9abb8ae
dacastro42 <108814849+dacastro42@users.noreply.github.com>
2025-02-28 15:53:01 -0500

    Initial commit

---
+++ src/model/ManejadorColores.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author 
+ */
+public interface ManejadorColores {
+    
+    public void agregarColor(String nombre, int r, int g, int b);
+    public void mostrarColoresOrdenados();
+    public void buscarColor(String nombre);
+    
+    
+}
---
+++ src/model/MapeoColoresRGB.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.LinkedList;
+
+/**
+ *
+ * @author 
+ */
+public class MapeoColoresRGB implements ManejadorColores{
+    
+    //defina el map que le permita siempre ordenar los colores por orden alfabetico 
+    
+    //constructor
+    public MapeoColoresRGB() {
+        // cree el map
+    }
+
+    @Override
+    public void agregarColor(String nombre, int r, int g, int b) {
+        
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void mostrarColoresOrdenados() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void buscarColor(String nombre) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+    //ordene una lista de string desde la Z hasta la A, mediante el método burbuja 
+    public LinkedList<String> ordenamientoBurbuja(LinkedList<String> listaNoOrdenada){
+        
+        return null;
+    }
+    
+}
